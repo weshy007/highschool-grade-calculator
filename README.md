e87c35270997ddf3e20915a1d941ac0dd769025b
weshy007 <josephwaweru96@gmail.com>
2021-02-25 18:51:14 +0300

    initial release

---
+++ README.md
@@ -0,0 +1,28 @@
+## SIMPLE STUDENT GRADING SYSTEM
+The system is a simple system which supposed to generate your Akan name and return it from the day you were born
+
+## Getting Started
+First you `git clone` the project into your machine and open it with the Virtual studio code. 
+So that is `git clone https://github.com/weshy007/grade-calculator.git` 
+
+## LAGUAGES USED
+- HTML
+- CSS 
+- JavaScript
+- Bootstrap
+
+
+## Behavior Driven Development(BDD)
+- The user is required to enter their subject grades and the system calculates their average marks.
+- The student is then prompted with their average marks and grade
+- They also get a message if they've passed or not
+
+
+## Author & Contacts
+Waithaka Waweru 
+- For Feedback josephwaweru96@gmail.com
+
+## Licence
+This project is licenced under the MIT Licence - see the [LICENCE](https://github.com/weshy007/grade-calculator/blob/master/LICENCE "Licence") file for details 
+- copyright 2021 - Waithaka Waweru 
+
---
+++ script.js
@@ -0,0 +1,69 @@
+function getmarks() {
+
+    var maths = Number(document.getElementById("maths").value);
+    var english = Number(document.getElementById("english").value);
+    var kiswahili = Number(document.getElementById("kiswahili").value);
+    var chemistry = Number(document.getElementById("chemistry").value);
+    var biology = Number(document.getElementById("biology").value);
+    var geography = Number(document.getElementById("geography").value);
+    var cre = Number(document.getElementById("religion").value);
+    var business = Number(document.getElementById("business").value);
+
+    var totalMarks = 800;
+    var MarksObtained = maths + english + kiswahili + chemistry + biology + geography + cre + business;
+    var percentage = (MarksObtained / totalMarks) * 100;
+    var grade = "";
+    document.getElementById("percentage").innerHTML = percentage + "%"
+
+    if (percentage > 80) {
+        grade = "A";
+    } 
+    else if (percentage < 79.99 && percentage > 75) {
+        grade = "A-";
+    } 
+    else if (percentage < 74.99 && percentage > 70) {
+        grade = "B+";
+    } 
+    else if (percentage < 69.99 && percentage > 65) {
+        grade = "B";
+    } 
+    else if (percentage < 64.99 && percentage > 60) {
+        grade = "B-";
+    }
+    else if (percentage < 59.99 && percentage > 55) {
+        grade = "C+";
+    }
+    else if (percentage < 54.99 && percentage > 50) {
+        grade = "C";
+    }
+    else if (percentage < 49.99 && percentage > 45) {
+        grade = "C-";
+    }
+    else if (percentage < 44.99 && percentage > 40) {
+        grade = "D+";
+    }
+    else if (percentage < 39.99 && percentage > 35) {
+        grade = "D";
+    }
+    else if (percentage < 34.99 && percentage > 30) {
+        grade = "D-";
+        }
+    else if (percentage < 29.99) {
+        grade = "E";
+    }
+
+    document.getElementById("grade").innerHTML = grade;
+    if (percentage < 29.99) {
+        document.getElementById("pass").innerHTML = "Sorry! you failed! Please try next time";
+    }
+    else if (percentage > 30 && percentage < 49.99) {
+        document.getElementById("pass").innerHTML = "You have to put more effort next time";
+    }
+    else if ( percentage > 50 && percentage < 60) {
+    document.getElementById("pass").innerHTML = "You can do better! Keep pushing";
+    }
+    else {
+        document.getElementById("pass").innerHTML = "Congratulations! you have passed with flying colors!";
+    }
+
+}
